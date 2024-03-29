73885d7c1b63cec28ab65be062dbfcb0fa410869
Evgeniy Sedov <i@evsedov.ru>
2024-03-30 01:37:31 +0700

    creates basic structure of project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://evsedov.github.io',
+  base: 'paulownia',
+})
