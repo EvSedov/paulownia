a0b68de6fe993588aeac0323c0d43b2b0faa885d
Evgeniy Sedov <sedoff-online@yandex.ru>
2024-04-13 21:58:32 +0700

    changes structure to multipage

--- vitejs/page.config.js
+++ vitejs/page.config.js
@@ -1,11 +1,11 @@
 import { resolve } from "path";
 
 const pages = [
-  { name: 'index', path: resolve(__dirname, '../index.js') },
-  { name: 'about', path: resolve(__dirname, '../pages/about.js') },
-  { name: 'contact', path: resolve(__dirname, '../pages/contact.js') },
-  { name: 'services', path: resolve(__dirname, '../pages/services.js') },
-  { name: 'portfolio', path: resolve(__dirname, '../pages/portfolio.js') },
+  { name: 'index', path: resolve(__dirname, '../index.html') },
+  { name: 'about', path: resolve(__dirname, '../pages/about.html') },
+  { name: 'contact', path: resolve(__dirname, '../pages/contact.html') },
+  { name: 'services', path: resolve(__dirname, '../pages/services.html') },
+  { name: 'portfolio', path: resolve(__dirname, '../pages/portfolio.html') },
 ];
 
 export default pages;
