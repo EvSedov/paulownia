7c6aecf2be1692d5499f03eef048f90cb4e26fee
Evgeniy Sedov <sedoff-online@yandex.ru>
2024-04-14 01:27:26 +0700

    fix vite config

--- vite.config.js
+++ vite.config.js
@@ -17,6 +17,23 @@ export default defineConfig({
             input: {
                 ...pagesInput,
             },
+            output: {
+                assetFileNames: ({ name }) => {
+                    if (/\.(gif|jpe?g|png|svg)$/.test(name ?? '')) {
+                        return 'assets/images/[name]-[hash][extname]'
+                    }
+
+                    if (/\.(ttf|otf|fnt|woff|woff2|eot)$/.test(name ?? '')) {
+                        return 'assets/fonts/[name]-[hash][extname]'
+                    }
+
+                    if (/\.css$/.test(name ?? '')) {
+                        return 'assets/css/[name]-[hash][extname]'
+                    }
+
+                    return 'assets/[name]-[hash][extname]'
+                },
+            },
         },
     },
     server: {
