3d3ba44eea3236efc963f45747df34f9e3885b95
Evgeniy Sedov <sedoff-online@yandex.ru>
2024-04-14 17:52:06 +0700

    changes base on vite config

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ import legacy from '@vitejs/plugin-legacy'
 // })
 
 export default defineConfig({
-    base: '/paulownia/',
+    base: '/',
     root: resolve(__dirname, 'src'),
     resolve: {
         alias: {
